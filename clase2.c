9dbf01160ed54220cc2d9ab9a78be6a7ce2f92d5
V1ct0r45 <125516286+V1ct0r45@users.noreply.github.com>
2023-02-20 22:39:47 +0000

    first commit

---
+++ clase2.c
@@ -0,0 +1,24 @@
+/**
+ * @file clase2.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-02-20
+ * 
+ * @copyright GPL v3
+ * 
+ */
+
+#include <stdio.h>
+#define PI 3.1416 //una constante
+
+int edad = 0; //variables groblales
+const float pi = 3.1416; // variables constantes
+
+int main()
+{
+    //int edade = 0; //variables locales 
+    //static int h=0 // se va a ejecutar pero solo una vez
+    
+    return 0;
+}
